af0022319ff24e775beaf54ee5f06ebd09885985
sunil <sunilc@netapp.com>
2023-07-28 19:31:12 +0530

    Updated resume

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Start Bootstrap - Resume v7.0.6 (https://sunilc18.github.io/sunilc-resume-site/)
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
